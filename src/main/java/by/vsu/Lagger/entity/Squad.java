4fa0b014a1fb1d8833cc4b3f4f829f81e14ef0ff
Ilya <https://ilyas_1999@bitbucket.org/ilyas_1999/diplom.git>
2017-05-13 19:15:57 +0300

    huinya

--- src/main/java/by/vsu/Lagger/entity/Squad.java
+++ src/main/java/by/vsu/Lagger/entity/Squad.java
@@ -34,14 +34,6 @@ public class Squad implements Serializable {
     @Column(name = "number")
     private Short number;
 
-    public Set<Child> getChildren() {
-        return children;
-    }
-
-    public void setChildren(Set<Child> children) {
-        this.children = children;
-    }
-
     @OneToMany(mappedBy = "squad", fetch = FetchType.LAZY)
     private Set<Child> children = new HashSet<>();
 
